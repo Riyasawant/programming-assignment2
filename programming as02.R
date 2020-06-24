506c3cb8d0217e76a8496e85b0fcdc4c6c052b12
Riyasawant <66208299+Riyasawant@users.noreply.github.com>
2020-06-24 20:55:28 +0530

    Update programming as02.R

--- programming as02.R
+++ programming as02.R
@@ -10,3 +10,19 @@ makeCacheMatrix <- function(x=matrix()) {
   getinv <-function() inv
   list(set=set, get=get,settinv=setinv,getinv=getinv)
   }
+# This function retrieves the inverse of the matrix cached from 
+##the makeCachematrix() environment
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+  
+    m <- x$getinverse()
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
+  }
